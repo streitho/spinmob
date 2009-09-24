bb03fdb906ecf76c515940fb25c115c4e626bd05
jack.sankey <jack.sankey@36434dd0-e1bb-11dd-95e1-755cbddbdd66>
2009-09-24 21:57:29 +0000

    fixed up some compatibility issues with the new version of matplotlib.
    (still using python2.5, by the way)

--- trunk/ spinmob/__init__.py
+++ trunk/ spinmob/__init__.py
@@ -15,7 +15,7 @@ except:
 
 import pylab
 pylab.ion()          # turn on interactive mode
-from pylab import gca, gcf, figure, axes
+from pylab import gca, gcf, figure, axes, draw
 
 import scipy
 import numpy
