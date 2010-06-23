b8c3221a04067231ea0195c54840c39ee427f32a
jack.sankey <jack.sankey@36434dd0-e1bb-11dd-95e1-755cbddbdd66>
2010-06-23 01:22:55 +0000

    * added plot.databoxes() * plot.files() returns list of databoxes
    * organized fitting
    * added fit.files(), fit.files2(), fit.shown_data()

---  spinmob/_plotting.py
+++  spinmob/_plotting.py
@@ -1,6 +1,7 @@
 import _pylab_tweaks as tweaks
 import _plotting_mess
 
+databoxes           = _plotting_mess.xy_databoxes
 files               = _plotting_mess.xy_files
 data                = _plotting_mess.xy
 surface_data        = _plotting_mess.xyz
