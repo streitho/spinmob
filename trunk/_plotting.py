cfbca96e950874f187cd67cc57452e76839a9112
jack.sankey <jack.sankey@36434dd0-e1bb-11dd-95e1-755cbddbdd66>
2010-08-06 02:00:22 +0000

    added function s.plot.function_parametric() for quickly plotting parametric curves in 2d.

--- trunk/_plotting.py
+++ trunk/_plotting.py
@@ -6,4 +6,5 @@ files               = _plotting_mess.xy_files
 data                = _plotting_mess.xy
 surface_data        = _plotting_mess.xyz
 function            = _plotting_mess.function_1D
+function_parametric = _plotting_mess.function_parametric
 surface_function    = _plotting_mess.function_2D
