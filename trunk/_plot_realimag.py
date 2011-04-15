c7e413267b6d923544702f25a8a3d3933deed2c2
jack.sankey <jack.sankey@36434dd0-e1bb-11dd-95e1-755cbddbdd66>
2011-04-15 16:39:16 +0000

    overhauled the plotting structure.

---
+++ trunk/_plot_realimag.py
@@ -0,0 +1,3 @@
+import _plotting_mess
+
+data        = _plotting_mess.realimag_data
---
+++ trunk/_plot_xy.py
@@ -0,0 +1,6 @@
+import _plotting_mess
+
+data        = _plotting_mess.xy_data
+databoxes   = _plotting_mess.xy_databoxes
+files       = _plotting_mess.xy_files
+function    = _plotting_mess.xy_function
